26e0cf4be9ed71a33653897e71506a47740f2284
antennae <junxiong.huang@stonybrook.edu>
2015-11-17 00:19:43 -0500

    fixed something

--- site/scripts.js
+++ site/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    $('.card').addClass('center-card');
     $('.one-time').slick({
         dots: true,
         infinite: true,
@@ -9,5 +9,39 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 3000
     });
+    if (document.documentElement.clientWidth <= 650) {
+        $('.col-xs-6').addClass('col-md-4').removeClass('col-xs-6');
+    } else {
+        $('.col-md-4').addClass('col-xs-6');
+    }
 
 });
+
+function loadTotables(fileName) {
+    d3.text(fileName, function (data) {
+        var parsedCSV = d3.csv.parseRows(data);
+
+        var container = d3.select(".tablehere")
+            .append("table")
+
+        .selectAll("tr")
+            .data(parsedCSV).enter()
+            .append("tr")
+
+        .selectAll("td")
+            .data(function (d) {
+                return d;
+            }).enter()
+            .append("td")
+            .text(function (d) {
+                return d;
+            });
+    });
+}
+$(window).resize(function () {
+    if (document.documentElement.clientWidth <= 650) {
+        $('.col-xs-6').addClass('col-md-4').removeClass('col-xs-6');
+    } else {
+        $('.col-md-4').addClass('col-xs-6');
+    }
+}).resize();
